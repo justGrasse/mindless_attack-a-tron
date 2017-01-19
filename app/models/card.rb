8563a660c5d2945d4f229cc4bb5e3d3537e716c2
Justin G <email": null>
2017-01-19 15:26:33 -0800

    create instance methods of Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,38 @@ class Card < ActiveRecord::Base
   has_many :decks, through: :decks_cards
 
   validates :card_id, presence: true
+
+  def name
+    card_data.name
+  end
+
+  def set
+    MTG::Set.find(set_initials).name
+  end
+
+  def power
+    card_data.power
+  end
+
+  def toughness
+    card_data.toughness
+  end
+
+  def image_url
+    card_data.image_url
+  end
+
+  def types
+    card_data.types
+  end
+
+  # private
+
+  def card_data
+    MTG::Card.find(card_id)
+  end
+
+  def set_initials
+    card_data.set
+  end
 end
