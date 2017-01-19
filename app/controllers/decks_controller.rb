bf98981e347bd207fc9dd7e4995e0e9cd86b450f
Justin G <email": null>
2017-01-19 11:21:37 -0800

    create deck show route

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -7,12 +7,21 @@ get '/decks/new' do
 end
 
 post '/decks' do
-  @deck = Deck.new( params[ :deck ] )
+  @deck = Deck.new params[:deck]
   if @deck.save
     redirect '/decks'
   else
     @errors = @deck.errors.full_messages
     erb :'decks/new'
+  end
+end
+
+get '/decks/:id' do
+  @deck = Deck.find params[:id]
+  if @deck
+    erb :'decks/show'
+  else
+    redirect :'/decks'
   end
 end
 
