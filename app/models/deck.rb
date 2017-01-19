49ecbe63573114ae6f17ed4c62a371964209323e
Justin G <email": null>
2017-01-19 15:31:43 -0800

    create deck instance method for selecting cards by type

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,11 @@
 class Deck < ActiveRecord::Base
   has_many :decks_cards
   has_many :cards, through: :decks_cards
+
+  def cards_by_type(type)
+    cards.select do |card|
+      card.types.include?(type)
+    end
+  end
+
 end
