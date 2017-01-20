6ba0e2de82b8af156359c3f5bd9236a301d68a1c
Justin G <email": null>
2017-01-20 12:42:03 -0800

    add graph and name instance methods for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ActiveRecord::Base
 
   before_validation :store_user_data, on: :create
 
+  def name
+    graph.get_object("me")['name']
+  end
+
+  def graph
+    Koala::Facebook::API.new(access_token)
+  end
+
   def store_user_data
 
   end
