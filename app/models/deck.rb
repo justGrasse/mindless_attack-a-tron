5d3efa922302a7659b33e929597583f13beae84f
Justin G <email": null>
2017-01-21 16:28:24 -0800

    create association between creator (User) and decks

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,7 @@
 class Deck < ActiveRecord::Base
   has_many :decks_cards
   has_many :cards, through: :decks_cards
+  belongs_to :creator, class_name: :User
 
   def cards_by_type(type)
     cards.select do |card|
---
+++ db/migrate/20170121161821_add_owner_key_to_decks_table.rb
@@ -0,0 +1,5 @@
+class AddOwnerKeyToDecksTable < ActiveRecord::Migration
+  def change
+    add_column :decks, :creator_id, :integer
+  end
+end
